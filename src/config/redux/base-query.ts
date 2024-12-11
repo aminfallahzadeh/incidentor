b76a7794fd7e828c877f05f1b2de61f4becb9737
amin-dev-tech <amin.dev.tech@gmail.com>
2024-12-11 22:16:24 +0300

    first deploy

--- src/config/redux/base-query.ts
+++ src/config/redux/base-query.ts
@@ -1,8 +1,8 @@
 // IMPORTS
-import { fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-import { RootState } from "./store";
-import type {
-  BaseQueryFn,
-  FetchArgs,
-  FetchBaseQueryError,
-} from "@reduxjs/toolkit/query";
+// import { fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+// import { RootState } from "./store";
+// import type {
+//   BaseQueryFn,
+//   FetchArgs,
+//   FetchBaseQueryError,
+// } from "@reduxjs/toolkit/query";
